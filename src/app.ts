d355e1b840fbc341d75406ed9b09a302ccea3750
Ricardo Velez <ricardovelecal@gmail.com>
2023-01-03 12:18:55 -0600

    mongodb conexion: ready

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import  express from "express";
 import cors from "cors";
 
 import { router } from "./routes";
+import db from "./config/mongo"
 
 const PORT = process.env.PORT;
 const app = express();
@@ -14,6 +15,9 @@ app.use(cors());
 
 // routes
 app.use(router);
+
+// mongodb conexion
+db().then(() => console.log('DB Conexion is ready'))
 
 // port
 app.listen(PORT, () => console.log(`Listen on port ${PORT}`));
