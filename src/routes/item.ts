dfeda86c81d466bd46845b28463594a17984a6a6
Ricardo Velez <ricardovelecal@gmail.com>
2023-01-03 12:59:03 -0600

    routes: created

---
+++ src/routes/item.ts
@@ -0,0 +1,12 @@
+import { Request, Response, Router } from "express";
+import { createItem, deleteItem, getItem, getItems, updateItem } from "../controllers/item";
+
+const router = Router();
+
+router.get('/', getItems);
+router.get('/:id', getItem);
+router.post('/', createItem);
+router.delete('/:id', deleteItem);
+router.put('/:id', updateItem);
+
+export { router };
